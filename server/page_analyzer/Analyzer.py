1de3a72c1ddd9312c7e6f70c092a0ed4e203eea3
Aidan Farhi <aidanfarhi@Aidans-MacBook-Air.local>
2021-02-03 18:10:59 -0500

    upgraded analyzer to use regex instead of word search and updated ReviewScraper headers to mimic my browser

--- server/page_analyzer/Analyzer.py
+++ server/page_analyzer/Analyzer.py
@@ -1,4 +1,11 @@
 from ReviewScraper import ReviewScraper
+import re
+
+test_urls = [
+    'https://www.amazon.com/ExcelSteel-Stainless-Steel-Colanders-Set/dp/B00FEDLBII/ref=cm_cr_arp_d_product_top?ie=UTF8&th=1',
+    'https://www.amazon.com/Headphones-Microphone-Lightweight-Comfortable-Foldable/dp/B07J5X68B2/ref=sr_1_1_sspa?crid=1LU42U7ZS8LUD&dchild=1&keywords=headphones&qid=1611168382&sprefix=headpho%2Caps%2C201&sr=8-1-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUExUDBIUzdZRURKUEdBJmVuY3J5cHRlZElkPUEwNDgwODk0TjlOM0lFMEZHVUE2JmVuY3J5cHRlZEFkSWQ9QTAyNDEzMzgyRlYwNDIzMk5aVTNMJndpZGdldE5hbWU9c3BfYXRmJmFjdGlvbj1jbGlja1JlZGlyZWN0JmRvTm90TG9nQ2xpY2s9dHJ1ZQ==',
+    'https://www.amazon.com/produce-aisle-Cantaloupe-One-Medium/dp/B000NSGULC?ref=US_TRF_DSK_UFG_RTL_ONSIT_0425559&fpw=fresh&pf_rd_r=DM5MK0RMDAHW6WNC5ZE7&pf_rd_p=a2fbb221-3d31-4621-bc2b-527067383c0e&pd_rd_r=a5320fa6-3922-4bd0-b37e-a246075cb449&pd_rd_w=Ur09B&pd_rd_wg=SedKi&ref_=pd_gw_unk'
+]
 
 class Analyzer:
     
@@ -7,34 +14,86 @@ class Analyzer:
         self.reviews = reviews
         # maintain a count of positive words found in reviews
         self.positive_phrases = {
-            "best": 0,
-            "love": 0,
-            "happy": 0,
-            "great": 0,
-            "high-quality": 0,
-            "excellent": 0,
+            'really like': 0,
+            'happy with this purchase': 0,
+            'happy with purchase': 0,
+            'love it': 0,
+            'love them': 0,
+            'love this': 0,
+            'good quality': 0,
+            'great quality': 0,
+            'glad I bought': 0,
+            'look great': 0,
+            'look good': 0,
+            'work perfectly': 0,
+            'work great': 0,
+            'work well': 0,
+            'glad I made': 0,
+            'good value': 0,
+            'great value': 0,
+            'sturdy': 0,
+            'well made': 0,
+            'good price': 0,
+            'great price': 0,
+            'excellent price': 0,
+            'perfect': 0,
+            'beautiful': 0,
+            'easy to use': 0,
+            'highly recommend': 0,
+            'would recommend': 0,
+            'great for the price': 0,
+            'good for price': 0,
+            'would highly recommend': 0,
         }
         self.total_positive_hits = 0
         # maintain a count of negagive words found in reviews
         self.negative_phrases = {
-            "hate": 0,
-            "disappointed": 0,
-            "poor": 0,
-            "bad": 0,
-            "dislike": 0
+            'try another brand': 0,
+            'returned': 0,
+            'wouldn’t buy again': 0,
+            'would not recommend': 0,
+            'don’t buy': 0,
+            'bad purchase': 0,
+            'don\'t waste your time': 0,
+            'don\'t waste your money': 0,
+            'don\'t be fooled': 0,
+            'dissatisfied': 0,
+            'low quality': 0,
+            'not satisfied': 0,
+            'poorly designed': 0,
+            'cheaply built': 0,
+            'cheaply made': 0,
+            'poor quality': 0,
+            'not made well': 0,
+            'waste of cash': 0,
+            'waste of money': 0,
+            'won\'t by again': 0,
+            'not worth the money': 0,
+            'disappointing purchase': 0,
+            'Junk': 0,
+            'not happy': 0,
+            'not a good choice': 0,
+            'returning': 0,
+            'annoying': 0,
+            'not high quality': 0,
+            'not sturdy': 0,
+            'not reliable': 0,
+            'unreliable': 0,
         }
         self.total_negative_hits = 0
 
     def get_counts(self, reviews):
         for review in reviews:
-            review = review.split(' ')
-            for word in review:
-                word = word.lower()
-                if word in self.positive_phrases:
-                    self.positive_phrases[word] += 1
+            review = review.lower()
+            for key in self.positive_phrases.keys():
+                reg = r'{key}'.format(key=key)
+                if re.search(reg, review) is not None:
+                    self.positive_phrases[key] += 1
                     self.total_positive_hits += 1
-                if word in self.negative_phrases:
-                    self.negative_phrases[word] += 1
+            for key in self.negative_phrases.keys():
+                reg = r'{key}'.format(key=key)
+                if re.search(reg, review) is not None:
+                    self.negative_phrases[key] += 1
                     self.total_negative_hits += 1
     
     def show_results(self):
@@ -49,7 +108,7 @@ class Analyzer:
 
 
 def main():
-    review_scraper = ReviewScraper()
+    review_scraper = ReviewScraper(test_urls[0])
     reviews = review_scraper.get_reviews()
     print(len(reviews))
     p_analyzer = Analyzer(reviews)
