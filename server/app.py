09435eda5ede4c53bce6008cc6f219eb73d4c6b2
Aidan Farhi <aidanfarhi@Aidans-MacBook-Air.local>
2021-02-04 18:47:32 -0500

    back end now recieving url

--- server/app.py
+++ server/app.py
@@ -1,6 +1,14 @@
-from flask import Flask
+from flask import Flask, request
+from page_analyzer import ReviewScraper, Analyzer
 
 app = Flask(__name__)
+
+@app.route('/analyze', methods=['POST'])
+def analyze():
+    if request.method == 'POST':
+        print(request.json['url'])
+        url = request.json['url']
+    return {"res": "Success"}
 
 @app.route('/hello')
 def hello():
