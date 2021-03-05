1b62ae96eb9b3c95c43ced296bd498045e759bbd
Aidan Farhi <aidanfarhi@Aidans-MacBook-Air.local>
2021-03-04 19:04:56 -0500

    attempt 7

--- server/app.py
+++ server/app.py
@@ -9,7 +9,7 @@ app = Flask(__name__, static_folder='../client/build', static_url_path='/')
 def not_found(e):
     return app.send_static_file('index.html')
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
     return app.send_static_file('index.html')
 
